78a7083a07104aad160f002f8393da6de05f3a56
Eric Chan <ericchan024@gmail.com>
2022-09-09 13:38:34 -0400

    created grid and ability to draw by clicking and dragging

---
+++ etch-a-sketch.js
@@ -0,0 +1,32 @@
+let isDrawing = false;
+
+const container = document.getElementById('container');
+
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement('div');
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = 'grid-item';
+    };
+};
+
+makeGrid(16, 16);
+
+const cells = document.querySelectorAll('.grid-item');
+cells.forEach((cell) => {
+    cell.addEventListener('mousedown', () => {
+        isDrawing = true;
+    });
+    cell.addEventListener('mousemove', () => {
+        if (isDrawing)
+            cell.classList.add('highlight');
+    });
+});
+
+window.addEventListener('mouseup', () => {
+    if (isDrawing) {
+        isDrawing = false;
+    }
+});
