fd5dd9d0a20829230650689f76a6ad557085f3d6
Eric Chan <ericchan024@gmail.com>
2022-09-13 14:21:11 -0400

    button functionality added

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,6 +3,14 @@ const container = document.getElementById('container');
 // get size of container
 let containerLength = getContentLength(container);
 
+function getContentLength (element) {
+    let styles = getComputedStyle(element)
+  
+    return element.clientWidth
+      - parseFloat(styles.paddingLeft)
+      - parseFloat(styles.paddingRight)
+}
+
 function createGrid(n) {
     container.style.setProperty('--grid-rows', n);
     container.style.setProperty('--grid-cols', n);
@@ -15,27 +23,44 @@ function createGrid(n) {
         cell.setAttribute('style', `width: ${cellLength}px; height: ${cellLength}px`);
         container.append(cell);
     }
-}
-createGrid(25);
-
-function getContentLength (element) {
-    let styles = getComputedStyle(element)
-  
-    return element.clientWidth
-      - parseFloat(styles.paddingLeft)
-      - parseFloat(styles.paddingRight)
+    draw();
 }
 
-const cells = document.querySelectorAll('.grid-item');
-cells.forEach((cell) => {
-    cell.addEventListener('mousemove', () => {
-        cell.classList.add('draw');
+function deleteGrid() {
+    const gridItems = document.querySelectorAll('.grid-item')
+    gridItems.forEach((item) => {
+        container.removeChild(item);
     });
-});
+}
+
+function draw() {
+    const cells = document.querySelectorAll('.grid-item');
+    cells.forEach((cell) => {
+        cell.addEventListener('mousemove', () => {
+            cell.classList.add('draw');
+        });
+    });
+}
 
 const reset = document.getElementById('reset');
 reset.addEventListener('click', () => {
-    cells.forEach((cell) => {
-        cell.classList.remove('draw');
+    const drawnCells = document.querySelectorAll('.draw');
+    drawnCells.forEach((drawnCell) => {
+        drawnCell.classList.remove('draw');
     });
 });
+
+const newGrid = document.getElementById('newGrid');
+newGrid.addEventListener('click', () => {
+    let size = prompt("Please enter the size of new grid", '16');
+    size =  parseInt(size);
+    deleteGrid();
+    createGrid(size);
+})
+
+// user clicks new grid
+// a prompt will come up to ask for size of grid
+// user inputs grid size
+// pass the input into the createGrid parameter
+
+createGrid(16);
